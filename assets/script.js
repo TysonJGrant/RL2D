f6a2cfe2f56000090ba829b3048cdb71e3fff35f
Tyson Grant <tysonjgrant@gmail.com>
2021-03-23 15:59:09 +1100

    First working controller commit2

--- assets/script.js
+++ assets/script.js
@@ -26,86 +26,9 @@ function start(){
     ypos+=ymove;
     socket.emit('update_cell', [xpos, ypos])
     if(data.users != null)
-      //redraw_game(data);
       draw_background();
       draw_player();
   })
-
-  function redraw_game(data){
-    //if(csize > 2000){ won = true; }   //win when big enough. do cool implode thing
-
-    ctx.clearRect(0, 0, c.width, c.height);
-    ctx.save();
-    ctx.translate(xoffset, yoffset);      //Draw with player in centre of screen
-    ctx.scale(scale_inc, scale_inc);
-    temp = data.users;
-    players = 0;
-    users = [];           //Stores all segments of all players
-    Object.keys(temp).forEach(function(key) {
-      players++;
-      segments = temp[key].segments;          //Get cell as array of segments
-      for(i = 0; i < segments.length; i++){
-        users.push(segments[i]);
-      }
-    });
-    users.sort(compare_size);     //Convert to sorted array by segment size
-
-    draw_background();
-
-    ctx.strokeStyle = "#555555";
-    ctx.lineWidth = 10;
-    ctx.beginPath();
-    ctx.rect(0, 0, 4000, 4000); //draw border
-    ctx.stroke();
-
-    food.forEach(function(item,i){              //Draw food
-      draw_circle(item[0], item[1], 5, '#fccd12');
-    });
-
-    pellets.forEach(function(item,i){              //Draw pellets
-      draw_circle(item.xpos, item.ypos, 20, '#5555ff');
-      draw_circle(item.xpos, item.ypos, 15, '#33ccff');
-    });
-
-    mines.forEach(function(item,i){              //Draw miness
-      draw_circle(item.xpos, item.ypos, 20, '#ff5555');
-      draw_circle(item.xpos, item.ypos, 15, '#ffcccc');
-    });
-
-    users.forEach(function(segment,i){   //Draw players
-      if(segment != null){
-        cel = images[segment.image];
-        ctx.drawImage(cel, 0, 0, cel.width, cel.height, segment.xpos-(segment.radius*10/2), segment.ypos-(segment.radius*10/2), segment.radius*10, segment.radius*10);
-        //draw_circle(player.xpos, player.ypos, 10, player.col);
-      }
-    });
-    ctx.restore();        //reset the transform
-
-    //document.getElementById("score").innerHTML = ("SIZE: &nbsp&nbsp" + csize + "  scale: " + scale + "  scale_inc: " + scale_inc + "<br>GOAL: 2000");
-    document.getElementById("players").innerHTML = ("PLAYERS: " + players);
-    document.getElementById("score").innerHTML = ("SIZE: &nbsp&nbsp" + parseInt(csize)); // + "<br>GOAL: 2000");
-    document.getElementById("pos").innerHTML = ("XPOS: " + parseInt(xpos) + "<br>YPOS: " + parseInt(ypos));
-    if(won){
-      scale_inc -= 0.002; //displaye win or lose and do invert explode thing. add start again button
-    }
-    else {
-      scale = (5+cradius/5)/cradius;//something about size
-      if(scale > 2) {
-        scale = 1.5;
-      }
-      else{
-        grow_amount = scale/200;
-        if(Math.abs(scale_inc - scale) < grow_amount)         //smooth out size changes
-          scale_inc = scale;
-        else if(scale_inc > scale){
-          scale_inc-=grow_amount;
-        }
-        else{
-          scale_inc+=grow_amount;
-        }
-      }
-    }
-  }
 
   function draw_circle(x, y, size, c){
     ctx.fillStyle = c;
